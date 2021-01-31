654958c3840d3622b488d91ddca2780234f086c8
unknown <vinhhoangngoc22@gmail.com>
2021-01-31 20:55:12 +0700

    KT BOI SO

---
+++ myscript.js
@@ -0,0 +1,8 @@
+function Test_Devide(){
+    let a = +prompt("Nhap vao so a");
+    let b = +prompt("Nhap vao so b");
+    if (a%b==0){
+        alert('a la boi cua b');
+    }
+    else alert('a khong la boi cua b')
+}
